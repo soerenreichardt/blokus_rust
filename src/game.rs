e034df9a368bf7fb90b284e47540fc82542aa221
Sören Reichardt <soren.reichardt@neo4j.com>
2024-06-01 15:53:07 +0200

    Implement proper piece placement checks

--- src/game.rs
+++ src/game.rs
@@ -1,27 +1,28 @@
 use std::collections::HashMap;
+
 use rand::random;
 use ratatui::style::Color;
 
 pub struct Game {
     pub(crate) board: Board,
-    players: Players
+    players: Players,
 }
 
 pub(crate) struct Board {
     pub(crate) width: u16,
     pub(crate) height: u16,
-    tiles: Vec<Vec<State>>
+    tiles: Vec<Vec<State>>,
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum State {
     Free,
-    Occupied(usize)
+    Occupied(usize),
 }
 
 pub struct Players {
     players: Vec<Player>,
-    active_player_index: usize
+    active_player_index: usize,
 }
 
 #[derive(Default, PartialEq)]
@@ -30,7 +31,7 @@ pub struct Player {
     pub color: Color,
     pub secondary_color: Color,
     pub available_pieces: Vec<Piece>,
-    pub first_move: bool
+    pub first_move: bool,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -39,20 +40,20 @@ pub struct Piece {
     pivot: f32,
     num_lines: u16,
     num_columns: u16,
-    pub(crate) bounding_box_offset: Position
+    pub(crate) bounding_box_offset: Position,
 }
 
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct Position {
     pub x: u16,
-    pub y: u16
+    pub y: u16,
 }
 
 impl Game {
     pub fn new(width: u16, height: u16, players: Players) -> Self {
         Game {
             board: Board::new(width, height),
-            players
+            players,
         }
     }
 
@@ -72,15 +73,18 @@ impl Game {
         &self.active_player().available_pieces
     }
 
-    pub fn place_piece(&mut self, piece_index: usize, rotations: u16, position: Position) -> Result<bool, String>{
+    pub fn place_piece(&mut self, piece_index: usize, rotations: u16, position: Position) -> Result<bool, String> {
         let player_index = self.players.active_player_index;
+        let first_round = self.active_player().first_move;
         let mut piece = self.active_player_mut().take_piece(piece_index);
+
         (0..rotations).for_each(|_| piece.rotate());
-        if let Some(piece) = self.board.place_piece(piece, position, player_index)? {
+        if let Some(piece) = self.board.place_piece(piece, position, player_index, first_round)? {
             self.return_piece_to_list(piece_index, rotations, piece);
-            return Ok(false)
+            return Ok(false);
         }
 
+        self.active_player_mut().first_move = false;
         self.switch_to_next_player();
         Ok(true)
     }
@@ -120,13 +124,13 @@ impl Board {
         Board {
             width,
             height,
-            tiles: vec![vec![State::Free; width as usize]; height as usize]
+            tiles: vec![vec![State::Free; width as usize]; height as usize],
         }
     }
 
-    fn place_piece(&mut self, piece: Piece, offset: Position, player_index: usize) -> Result<Option<Piece>, String> {
-        if !self.piece_can_be_placed(&piece, &offset) {
-            return Ok(Some(piece))
+    fn place_piece(&mut self, piece: Piece, offset: Position, player_index: usize, first_round: bool) -> Result<Option<Piece>, String> {
+        if !self.piece_can_be_placed(&piece, &offset, player_index, first_round) {
+            return Ok(Some(piece));
         }
 
         for local_position in piece.blocks() {
@@ -147,16 +151,90 @@ impl Board {
         Ok(())
     }
 
-    fn piece_can_be_placed(&self, piece: &Piece, offset: &Position) -> bool {
-        for local_position in piece.blocks() {
-            let board_position = &local_position + offset;
-            match self.get_state_on_position(&board_position).unwrap() {
-                State::Free => (),
-                State::Occupied(_) => return false
+    fn piece_can_be_placed(&self, piece: &Piece, offset: &Position, player_index: usize, first_round: bool) -> bool {
+        let can_be_placed_in_first_round = piece.blocks()
+            .map(|block| &block + offset)
+            .all(|position| self.block_position_is_not_occupied(&position)
+                    && self.block_is_not_adjacent_to_other_blocks_from_same_player(&position, player_index));
+
+        if !first_round {
+            let is_diagonally_adjacent = piece.blocks()
+                .map(|block| &block + offset).find(|position| self.block_is_diagonally_adjacent_to_block_from_same_player(position, player_index))
+                .is_some();
+            return is_diagonally_adjacent && can_be_placed_in_first_round;
+        }
+        can_be_placed_in_first_round
+    }
+
+    fn block_position_is_not_occupied(&self, position: &Position) -> bool {
+        match self.get_state_on_position(position).unwrap() {
+            State::Free => true,
+            State::Occupied(_) => false
+        }
+    }
+
+    fn block_is_not_adjacent_to_other_blocks_from_same_player(&self, position: &Position, player_index: usize) -> bool {
+        if position.x > 0 {
+            match self.get_state_on_position(&Position { x: position.x - 1, y: position.y }).unwrap() {
+                State::Occupied(index) if index == player_index => return false,
+                _ => ()
+            }
+        }
+
+        if position.x < self.width - 1 {
+            match self.get_state_on_position(&Position { x: position.x + 1, y: position.y }).unwrap() {
+                State::Occupied(index) if index == player_index => return false,
+                _ => ()
+            }
+        }
+
+        if position.y > 0 {
+            match self.get_state_on_position(&Position { x: position.x, y: position.y - 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return false,
+                _ => ()
+            }
+        }
+
+        if position.y < self.height - 1 {
+            match self.get_state_on_position(&Position { x: position.x, y: position.y + 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return false,
+                _ => ()
             }
         }
 
         true
+    }
+
+    fn block_is_diagonally_adjacent_to_block_from_same_player(&self, position: &Position, player_index: usize) -> bool {
+        if position.x > 0 && position.y > 0 {
+            match self.get_state_on_position(&Position { x: position.x - 1, y: position.y - 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return true,
+                _ => ()
+            }
+        }
+
+        if position.x < self.width - 1 && position.y > 0 {
+            match self.get_state_on_position(&Position { x: position.x + 1, y: position.y - 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return true,
+                _ => ()
+            }
+        }
+
+        if position.x > 0 && position.y < self.height - 1 {
+            match self.get_state_on_position(&Position { x: position.x - 1, y: position.y + 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return true,
+                _ => ()
+            }
+        }
+
+        if position.x < self.width - 1 && position.y < self.height - 1 {
+            match self.get_state_on_position(&Position { x: position.x + 1, y: position.y + 1 }).unwrap() {
+                State::Occupied(index) if index == player_index => return true,
+                _ => ()
+            }
+        }
+
+        false
     }
 }
 
@@ -181,7 +259,7 @@ impl std::ops::Add for Position {
     fn add(self, rhs: Self) -> Self::Output {
         Position {
             x: self.x + rhs.x,
-            y: self.y + rhs.y
+            y: self.y + rhs.y,
         }
     }
 }
@@ -192,7 +270,7 @@ impl std::ops::Add for &Position {
     fn add(self, rhs: Self) -> Self::Output {
         Position {
             x: self.x + rhs.x,
-            y: self.y + rhs.y
+            y: self.y + rhs.y,
         }
     }
 }
@@ -203,7 +281,7 @@ impl std::ops::Sub for &Position {
     fn sub(self, rhs: Self) -> Self::Output {
         Position {
             x: self.x - rhs.x,
-            y: self.y - rhs.y
+            y: self.y - rhs.y,
         }
     }
 }
@@ -218,7 +296,7 @@ impl Piece {
         Piece { blocks, pivot, num_lines, num_columns, bounding_box_offset }
     }
 
-    pub fn blocks(&self) -> impl Iterator<Item = Position> + '_ {
+    pub fn blocks(&self) -> impl Iterator<Item=Position> + '_ {
         self.blocks.iter().map(|block| block - &self.bounding_box_offset)
     }
 
@@ -229,7 +307,7 @@ impl Piece {
         std::mem::swap(&mut self.num_columns, &mut self.num_lines);
         self.bounding_box_offset = Position {
             x: Self::min_x(&self.blocks),
-            y: Self::min_y(&self.blocks)
+            y: Self::min_y(&self.blocks),
         }
     }
 
@@ -265,7 +343,7 @@ impl Players {
         let active_player_index = random::<usize>() % players.len();
         Players {
             players,
-            active_player_index
+            active_player_index,
         }
     }
 
@@ -281,7 +359,7 @@ impl Player {
             color,
             secondary_color,
             available_pieces,
-            first_move: true
+            first_move: true,
         }
     }
 
@@ -299,7 +377,7 @@ mod tests {
     use super::*;
 
     fn piece_1x1() -> Piece {
-        Piece::new(vec![Position { x: 0, y : 0 }], 0.0)
+        Piece::new(vec![Position { x: 0, y: 0 }], 0.0)
     }
 
     #[test]
@@ -319,10 +397,10 @@ mod tests {
         let mut piece = Piece::new(vec![Position { x: 0, y: 1 }, Position { x: 1, y: 1 }, Position { x: 2, y: 1 }], 1.0);
 
         piece.rotate();
-        assert_eq!(piece.blocks, vec![Position { x: 1, y: 0}, Position { x: 1, y: 1}, Position { x: 1, y: 2}]);
+        assert_eq!(piece.blocks, vec![Position { x: 1, y: 0 }, Position { x: 1, y: 1 }, Position { x: 1, y: 2 }]);
 
         piece.rotate();
-        assert_eq!(piece.blocks, vec![Position { x: 2, y: 1}, Position { x: 1, y: 1}, Position { x: 0, y: 1}]);
+        assert_eq!(piece.blocks, vec![Position { x: 2, y: 1 }, Position { x: 1, y: 1 }, Position { x: 0, y: 1 }]);
     }
 
     #[test]
